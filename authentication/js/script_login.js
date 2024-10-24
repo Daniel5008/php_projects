588458b1f0cff8d4508f1bf2edf5bfdd89c83672
Daniel Zerwes Hunsche <daniel.hunsche@gmail.com>
2024-10-23 21:43:23 -0300

    removido pasta authorization, será criado novamente o projeto

--- authentication/js/script_login.js
+++
@@ -1,6 +0,0 @@
-function toggleForms() {
-    const loginContainer = document.getElementById('login-container');
-    const registerContainer = document.getElementById('register-container');
-    loginContainer.style.display = loginContainer.style.display === 'none' ? 'block' : 'none';
-    registerContainer.style.display = registerContainer.style.display === 'none' ? 'block' : 'none';
-}
